d80f39644417b85b130ebeca6aa807a9b01cf66a
Ilyas Shagaleev <ilyas1996shagaleev@gmail.com>
2024-10-22 00:25:52 +0500

    initial commit

---
+++ test_url_validate.py
@@ -0,0 +1,8 @@
+import re
+from playwright.sync_api import Page, expect # type: ignore
+
+def test_has_valid_url(page: Page):
+    page.goto("https://gh-users-search.netlify.app/")
+    # Проверка корректности урла
+    expect(page).to_have_url("https://gh-users-search.netlify.app/")
+
